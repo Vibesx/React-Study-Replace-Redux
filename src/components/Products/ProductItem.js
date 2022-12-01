c8f4a291d299792d23e512f129fbb2a8e14df432
vibesx <lucian.paslariu@gmail.com>
2022-12-01 11:58:56 +0200

    ...

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,28 +1,31 @@
-import React from "react";
+import React, { useContext } from 'react';
 
-import Card from "../UI/Card";
-import "./ProductItem.css";
-import { toggleFav } from "../../store/actions/products";
+import Card from '../UI/Card';
+import { ProductsContext } from '../../context/products-context';
+import './ProductItem.css';
 
-const ProductItem = (props) => {
-	const toggleFavHandler = () => {
-		// dispatch(toggleFav(props.id));
-	};
+const ProductItem = props => {
 
-	return (
-		<Card style={{ marginBottom: "1rem" }}>
-			<div className="product-item">
-				<h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
-				<p>{props.description}</p>
-				<button
-					className={!props.isFav ? "button-outline" : ""}
-					onClick={toggleFavHandler}
-				>
-					{props.isFav ? "Un-Favorite" : "Favorite"}
-				</button>
-			</div>
-		</Card>
-	);
+  const toggleFav = useContext(ProductsContext).toggleFav;
+
+  const toggleFavHandler = () => {
+    toggleFav(props.id);
+  };
+
+  return (
+    <Card style={{ marginBottom: '1rem' }}>
+      <div className="product-item">
+        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+        <p>{props.description}</p>
+        <button
+          className={!props.isFav ? 'button-outline' : ''}
+          onClick={toggleFavHandler}
+        >
+          {props.isFav ? 'Un-Favorite' : 'Favorite'}
+        </button>
+      </div>
+    </Card>
+  );
 };
 
 export default ProductItem;
--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,27 +1,24 @@
-import React, { useContext } from "react";
-import { useSelector } from "react-redux";
+import React, { useContext } from 'react';
 
-import { ProductsContext } from "../context/products-context";
+import ProductItem from '../components/Products/ProductItem';
+import { ProductsContext } from '../context/products-context';
+import './Products.css';
 
-import ProductItem from "../components/Products/ProductItem";
-import "./Products.css";
-
-const Products = (props) => {
-	const productsContext = useContext(ProductsContext);
-	const productList = productsContext.products;
-	return (
-		<ul className="products-list">
-			{productList.map((prod) => (
-				<ProductItem
-					key={prod.id}
-					id={prod.id}
-					title={prod.title}
-					description={prod.description}
-					isFav={prod.isFavorite}
-				/>
-			))}
-		</ul>
-	);
+const Products = props => {
+  const productList = useContext(ProductsContext).products;
+  return (
+    <ul className="products-list">
+      {productList.map(prod => (
+        <ProductItem
+          key={prod.id}
+          id={prod.id}
+          title={prod.title}
+          description={prod.description}
+          isFav={prod.isFavorite}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default Products;
