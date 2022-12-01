dd04bbcf90caa4188e8e2dae77f461c004e1f849
vibesx <lucian.paslariu@gmail.com>
2022-12-01 12:48:41 +0200

    implemented a custom hook to manage global state/context

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import ProductsProvider from './context/products-context';
+import "./index.css";
+import App from "./App";
+import ProductsProvider from "./context/products-context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import configureStore from "./hooks-store/products-store";
+
+configureStore();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
